ec4266db6bcc1da3148cd6563e68429f181c9a49
jsk3342 <kss2460@naver.com>
2022-02-21 22:13:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,29 @@ function 반복문() {
 }
 
 반복문();
+
+function 더하기(a, b) {
+  return a + b;
+}
+function 빼기(a, b) {
+  return a - b;
+}
+
+function calculate(command, a, b) {
+  switch (command) {
+    case "add":
+      return a + b;
+    case "substract":
+      return a - b;
+    case "divide":
+      return a / b;
+    case "multiply":
+      return a * b;
+    case "remainder":
+      return a % b;
+    default:
+      throw Error("unkown command");
+  }
+}
+
+console.log(calculate("add", 3, 5)); //8
