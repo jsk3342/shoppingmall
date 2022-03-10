bed33ee1db26a9c6994cc77d662df5fc55883afb
jsk3342 <kss2460@naver.com>
2022-03-10 21:09:02 +0900

    a

--- string.c
+++ string.c
@@ -1,8 +1,16 @@
 
 int main(void)
 {
-    for (int i = 0; i < 10; i ++;)
+    for (int i = 0; i < 10; i ++)
     {
         printf("개발공부는 재미있다!\n");
     }
+}
+int main(void){
+    string s = get_string("Input: ");
+    printf("Output: ");
+        for (int i = 0; s.length; i++) {
+            printf("%c", s[i]);
+        }
+    printf("/n");
 }
